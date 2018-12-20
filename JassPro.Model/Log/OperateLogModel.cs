84252407f548f5b7ad8f040fca18d6042817f3c5
Allen <18781976857@163.com>
2018-12-20 21:11:45 +0800

    m

---
+++ JassPro.Model/Log/OperateLogModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JassPro.Model.Log
+{
+    [Serializable]
+    [Table("system_operate_log")]
+    public class OperateLogModel : BaseModel
+    {
+        [Column("join_id")]
+        public int JoinId { get; set; }
+
+        [Column("join_name")]
+        public string JoinName { get; set; }
+
+        [Column("store_id")]
+        /// <summary>
+        /// 店铺id
+        /// </summary>
+        public int StoreId { get; set; }
+
+        [Column("user_id")]
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public int UserId { get; set; }
+
+        [Column("user_name")]
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public string UserName { get; set; }
+
+        [Column("description")]
+        /// <summary>
+        /// 操作内容
+        /// </summary>
+        public string Description { get; set; }
+
+        [Column("extra")]
+        /// <summary>
+        /// 额外参数(json)
+        /// </summary>
+        public string Extra { get; set; }
+
+        [Column("add_time")]
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public string AddTime { get; set; }
+    }
+
+    public class OperateLogRequest : BaseQueryModel
+    {
+        public int JoinId { get; set; }
+        public int StoreId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Description { get; set; }
+
+        public string StartTime { get; set; }
+
+        public string EndTime { get; set; }
+    }
+}
